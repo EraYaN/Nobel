163df8b9eb01aa18925ba7e2a5d31fcaff6bc19d
Erwin de Haan <git@erayan.eu>
2013-03-08 22:30:06 +0100

    Added model.

---
+++ DatabaseModels/Archief.sql
@@ -0,0 +1,38 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+
+-- -----------------------------------------------------
+-- Table `dehaantj_dsbarchief`.`documenten`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `dehaantj_dsbarchief`.`documenten` (
+  `document_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `titel` VARCHAR(255) NULL ,
+  `omschrijving` TEXT NULL ,
+  `datum` DATE NULL ,
+  `datum_nauwkeurigheid` ENUM('jaar','maand','dag') NULL ,
+  `datum_archivering` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  `type` SET('hardcopy','digitaal') NOT NULL DEFAULT 1 ,
+  `locatie_file` VARCHAR(255) NULL COMMENT 'relatief tot project master.' ,
+  `locatie_hc` BIGINT UNSIGNED NULL COMMENT 'locatie als in map id' ,
+  PRIMARY KEY (`document_id`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dehaantj_dsbarchief`.`mappen`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `dehaantj_dsbarchief`.`mappen` (
+  `map_id` INT NOT NULL ,
+  `kast` VARCHAR(45) NULL ,
+  `titel` VARCHAR(45) NULL ,
+  `omschrijving` TEXT NULL ,
+  PRIMARY KEY (`map_id`) )
+ENGINE = InnoDB;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;

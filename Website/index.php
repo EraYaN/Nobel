9074c285f51934223ea298ef9b1b5e355129b554
Erwin de Haan <git@erayan.eu>
2014-02-18 14:58:57 +0100

    Website

---
+++ Website/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>HJG Nobel</title>
+<link rel="shortcut icon" href="/favicon.png" type="image/png">
+</head>
+
+<body>
+<img src="favicon.png" alt="Logo" style="float:left; height: auto; width: auto; margin-right: 40px;">
+<h1 style=" font-family:Castellar, Georgia, 'Times New Roman', Times, serif; font-size: 6em; font-weight: normal; padding-top:56px; margin-top:10px; margin-bottom:0px;">HJG Nobel</h1>
+<h2 style=" font-family:Castellar, Georgia, 'Times New Roman', Times, serif; font-size: 2em; font-weight: normal; padding-top:0px; margin-top:10px;">Opgericht 10 oktober 2010</h2>
+<div style="clear:both;"></div>
+<p>Please come back later. Nothing here.</p>
+</body>
+</html>
